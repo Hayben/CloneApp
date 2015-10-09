214d5343fb360344bdc62f8d4ea74d006cfd4b46
123 <123@123.local>
2015-10-09 14:37:13 +0800

    Initial Commit

---
+++ YZYApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YZYApp
+//
+//  Created by 123 on 15/10/9.
+//  Copyright © 2015年 haibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YZYApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YZYApp
+//
+//  Created by 123 on 15/10/9.
+//  Copyright © 2015年 haibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
